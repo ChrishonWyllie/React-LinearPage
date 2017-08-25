33950055a7ddd0a72e2d28e088cc70dfd774794b
Chrishon Wyllie <chrishon595@yahoo.com>
2017-08-25 13:57:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ReactTest | Generated from React</title>
+		
+		<!-- This enables bootstrap responsive design features on mobile devices -->
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css">
+		
+		<!---jQuery CDN-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		
+		
+		
+		<!-- Custom JS -->
+	    <script type="text/jsx" src="../potburn_test_environment/js/index.js"></script>
+	    <script type="text/jsx" src="../potburn_test_environment/js/index.js"></script>
+	
+		<!-- Custom CSS rules-->
+		<link rel ="stylesheet" href="../potburn_test_environment/css/index.css">
+		<link rel ="stylesheet" href="../potburn_test_environment/css/footer.css">
+		
+	</head>
+	
+	<body>
+	
+		<div id="navbar-init"></div>
+	
+		<div id="div-area-one">
+		<!-- Handled with React.js -->
+		</div>
+		
+		<div id="div-area-two">
+		<!-- Handled with React.js -->
+		</div>
+		
+		<div id="div-area-three">
+		<!-- Handled with React.js -->
+		</div>
+		
+		<div id="div-area-four">
+		<!-- Handled with React.js -->
+		</div>
+	
+	
+		<footer id="footer">
+		<!-- handled with React.js -->
+		</footer>
+
+
+
+	
+	<!--Bootstrap Js CDN-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- React | ReactDOM | React-Bootstrap JSXTransformer CDN -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.1/react.min.js"></script>
+	<srcipt src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.1/react-dom.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/react-bootstrap/0.31.2/react-bootstrap.min.js.map"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/JSXTransformer.js"></script>
+	
+	</body>
+</html>
